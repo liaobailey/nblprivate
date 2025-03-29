1e040b3d32c11023c3a1a7163d2be6f0d94e2487
liaobailey <47491229+liaobailey@users.noreply.github.com>
2025-03-29 02:37:54 -0400

    Update fourfactors.py

--- fourfactors.py
+++ fourfactors.py
@@ -64,8 +64,8 @@ away = away_pre.merge(rnd, on = ['TEAM', 'GAMEID'], how = 'left')
 home = home_pre.merge(rnd, on = ['TEAM', 'GAMEID'], how = 'left')
 
 
-away['DATE_format'] = pd.Timestamp(away['DATE_x']).to_pydatetime()
-home['DATE_format'] = pd.Timestamp(home['DATE_x']).to_pydatetime()
+away['DATE_format'] = away['DATE_x'].map(lambda x: pd.Timestamp(x).to_pydatetime())
+home['DATE_format'] = home['DATE_x'].map(lambda x: pd.Timestamp(x).to_pydatetime())
 
 
 
