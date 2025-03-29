624a91403df35d3b5735a07c0b4c755e86e74fd3
liaobailey <47491229+liaobailey@users.noreply.github.com>
2025-03-29 02:10:06 -0400

    Update fourfactors.py

--- fourfactors.py
+++ fourfactors.py
@@ -138,13 +138,9 @@ display_df = clean[['TEAM_self', 'off_EFG_rank', 'off_TOV_rank', 'off_OREB_rank'
        'def_TOV_rank', 'def_OREB_rank', 'def_FTR_rank']]
 
 display_df.iloc[:, 1:] = display_df.iloc[:, 1:].round(2)
-
-# Set pandas display options to ensure all floating-point numbers display with 2 decimals
-pd.set_option('display.float_format', '{:.2f}'.format)
-
-
 display_df.columns = ['Team Name', 'Off EFG% Rank', 'Off TOV% Rank', 'Off OREB% Rank', 'Off FTR Rank', 'Def EFG% Rank', 'Def TOV% Rank', 'Def OREB% Rank', 'Def FTR Rank']
 styled_df = display_df.style.background_gradient(cmap='coolwarm', subset=['Off EFG% Rank', 'Off TOV% Rank', 'Off OREB% Rank', 'Off FTR Rank', 'Def EFG% Rank', 'Def TOV% Rank', 'Def OREB% Rank', 'Def FTR Rank'])
+styled_df = styled_df.format({col: '{:.2f}' for col in display_df.columns[1:]})
 
-st.write(styled_df)
+st.dataframe(styled_df)
 
